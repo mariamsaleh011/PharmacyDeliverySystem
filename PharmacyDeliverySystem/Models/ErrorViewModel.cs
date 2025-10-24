12ed503d62f40fd4e7f0f095268d461b2c5e8259
mariamislam <mariamsaleh5544icloud.com@mariamislams-MacBook-Air.local>
2025-10-24 22:16:00 +0300

    Initial commit

---
+++ PharmacyDeliverySystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PharmacyDeliverySystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
