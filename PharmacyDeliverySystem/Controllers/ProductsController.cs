e8d07a46cddce6ff5a31c10ef5ed1aa8bd80ccca
Omar Elsayed <momoahead89@gmail.com>
2025-12-02 16:00:36 +0200

    Add Page Product

---
+++ PharmacyDeliverySystem/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PharmacyDeliverySystem.DataAccess;
+using PharmacyDeliverySystem.Models;
+
+namespace PharmacyDeliverySystem.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly PharmacyDeliveryContext _context;
+
+        public ProductsController(PharmacyDeliveryContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            ViewBag.ProductsList = products;
+            return View();
+        }
+
+    }
+}
