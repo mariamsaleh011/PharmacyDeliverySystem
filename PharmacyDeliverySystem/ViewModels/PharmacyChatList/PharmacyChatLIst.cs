f64784532dcae1ed9af232954b98a336175a5831
Jana Mohamed <JanaM7151@gmail.com>
2025-12-03 13:37:57 +0200

    valid Chatting Between Customers and Pharmacists

---
+++ PharmacyDeliverySystem/ViewModels/PharmacyChatList/PharmacyChatLIst.cs
@@ -0,0 +1,10 @@
+﻿namespace PharmacyDeliverySystem.ViewModels
+{
+    public class PharmacyChatListItemViewModel
+    {
+        public int ChatId { get; set; }
+        public string CustomerName { get; set; } = "";
+        public string LastMessage { get; set; } = "";
+        public DateTime? LastMessageTime { get; set; }
+    }
+}
